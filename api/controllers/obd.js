7833074bebe3ac2be7d14d25040e67353aa9aa93
Javier Suarez <mozkysl@gmail.com>
2018-06-05 23:18:15 -0500

    alarma funcionando

--- api/controllers/obd.js
+++ api/controllers/obd.js
@@ -1,127 +1,85 @@
-'use strict';
+'use strict'
 
-const util = require('util');
 const admin = require('firebase-admin')
 const FCM = require('fcm-node')
 const turf = require ('turf')
+const util = require('util')
 
-const Vehiculo = require('../models/vehiculo');
-const config = require('../../config'); // get our config file
-const Obd = require('../models/obd');
-const Ubicacion = require('../models/ubicacion.js');
+const Vehiculo = require('../models/vehiculo')
+const config = require('../../config') // get our config file
+const Obd = require('../models/obd')
+const Ubicacion = require('../models/ubicacion.js')
 const Alarma = require('../models/alarma.js')
 const User = require('../models/user.js')
 const UbicacionFav = require('../models/ubicacionesFavoritas.js')
-var fcm = new FCM('AAAAyfccUwk:APA91bHAfibQ-MxQBEPWivTZvovyPPKHLlaVquksczuJJLJsgHtIEk0Md4E2Ia8M4kDm9qlcQGjy2eigkqomS-C_Ze_BIe9zoJp1fiqJ4WfCJACSOzltBGPbte_69TPTvfSkXnKzDrf9');
+
+// TODO: Add this to config file
+const CheckAlarmsAndSend = require('../helpers/alarm_sender')
+const fcm = new FCM('AAAAyfccUwk:APA91bHAfibQ-MxQBEPWivTZvovyPPKHLlaVquksczuJJLJsgHtIEk0Md4E2Ia8M4kDm9qlcQGjy2eigkqomS-C_Ze_BIe9zoJp1fiqJ4WfCJACSOzltBGPbte_69TPTvfSkXnKzDrf9')
+
+
 module.exports = {
-    addOBD: function(req, res){
-        const obdObject = req.swagger.params.obd.value;
-        const ubicacion = JSON.parse('{"ubicacion":{"smidivID":"ABC123","lat":0,"lng":0}}');
-        if (!obdObject) res.status(400).json('Error');
-        const vehiculo = Vehiculo.findOne({
-            'smidivID': obdObject.smidivID
+    addOBD: function(req, res) {
+        const obdObject = req.swagger.params.obd.value
+        if (!obdObject) res.status(400).json('Error')
+
+        Vehiculo.findOne({
+            smidivID: obdObject.smidivID
         })
         .exec(function (err, vehiculo) {
-            console.log("vehiculo"+vehiculo);
             if (err || !vehiculo) {
                 console.log('Error fetching vehiculo, #addUbicacion', err)
-                res.status(400).json('Error fetching vehicle')
-                return;
+                res.status(400).json(`Error encontrando el vehículo del SMIDIV ID: ${obdObject.smidivID}`)
+                return
             }
-            obdObject.vehiculo=vehiculo._id;
-            obdObject.idAutomovil=vehiculo._id;
-            ubicacion.idAutomovil=vehiculo._id;
            
-            obdObject.PID.map(function(x){
-                if(x.lat){    
-                    
-                    ubicacion.ubicacion.lat = x.lat;
-                    ubicacion.ubicacion.lng= x.lng; 
-                    const ala = Alarma.findOne({
-                        "usuario": vehiculo.usuario
-                        }).exec(function(err,alarm){
-                            if(err){
-                                console.log('Error fetching vehiculo, #addUbicacion', err)
-                                res.status(400).json('Error encontrando usuario')
-                            }
-                            console.log("alarma   "+alarm);
-                            const ub= UbicacionFav.findOne({
-                                "idusuario":vehiculo.usuario
-                            })
-                            .exec(function(err, fav){
-                                if(err){
-                                    console.log('Error fetching vehiculo, #addUbicacion', err)
-                                    res.status(400).json('Error encontrando usuario')
-                                }
-                                console.log(fav);
-                                var uno = turf.point([fav.ubicacion.lat, fav.ubicacion.lng]);
-                                //console.log(fav);
-                                var dos = turf.point([ubicacion.ubicacion.lat, ubicacion.ubicacion.lng]);
-                                var options = {units: 'kilometers'};
-                                var distance = turf.distance(uno, dos, 'kilometers');
-                                if (distance>alarm.rangoDistancia.rango){
-                                    const Usuario = User.findOne({
-                                        "_id":vehiculo.usuario
-                                    }).exec(function(err,topic){
-                                        var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-                                            to: '/topics/'+topic.username,
-                                            
-                                            notification: {
-                                                title: 'Notificacion SMIDIV!', 
-                                                body: 'Tu Vehiculo ha salido de el rango definido' 
-                                            },
-                                            
-                                            data: {  //you can send only notification or only data(or include both)
-                                                my_key: 'my value',
-                                                my_another_key: 'my another value'
-                                            }
-                                        };
-                                        fcm.send(message, function(err, response){
-                                            if (err) {
-                                                console.log("Something has gone wrong!");
-                                            } else {
-                                                console.log("Successfully sent with response: ", response);
-                                            }
-                                        });
-                                    });     
-                            }
-                        })
-                    }); 
-                    console.log("hola");
-                    new Ubicacion(ubicacion).save(function (err, nuevaUbicacion) {
+            obdObject.PID.map(x => {
+                if (x.lat) {
+                    const ubicacionObject = {
+                        idAutomovil: vehiculo._id,
+                        fechaCreacion: new Date(),
+                        ubicacion: {
+                            lat: x.lat,
+                            lng: x.lng,
+                        }
+                    }
+
+                    new Ubicacion(ubicacionObject).save(function (err, nuevaUbicacion) {
                         if (err) {
                             if (err.code == 11000) res.status(400).json('Ubicacion ya guardada')
                             return console.error(err)
                         } else {
                             console.log('New ubicacion saved', nuevaUbicacion)
+                            CheckAlarmsAndSend(ubicacionObject.idAutomovil, vehiculo.usuario, nuevaUbicacion)
                         }
                     })
-
-                }
-
-
-                obdObject.tipo = x.tipo;
-                obdObject.valor = x.valor;
-                new Obd(obdObject).save(function(err,nobd){
-                    if (err) {
-                        if (err.code == 11000) res.status(400).json('Ubicacion ya guardada');
-                        return console.error(err);
-                    } else {
-                        console.log('New obd saved', nobd)
+                } else if (x.tipo) {
+                    const obd = {
+                        smidivID: obdObject.smidivID,
+                        tipo: x.tipo,
+                        valor: x.valor
                     }
-                })
+    
+                    new Obd(obd).save(function(err, newOBD){
+                        if (err) {
+                            if (err.code == 11000) res.status(400).json('Ubicacion ya guardada')
+                            return console.error(err)
+                        } else {
+                            console.log('New obd saved', newOBD)
+                        }
+                    })
+                }
             })
+
             res.json({
                 success: true,
                 response: {
-                    PID:obdObject.PID
+                    PID: ''
                 }
-            });
-
-        });
-
+            })
+        })
     },
-    getOBD: function(req, res){
+    getOBD: function(req, res) {
         //var obdobject = req.swagger.params.vehiculo.value
         const obd = Vehiculo.findOne({
             'placas': req.swagger.params.vehiculo.value
@@ -131,25 +89,24 @@ module.exports = {
             if (err || !vehi) {
                 console.log('Error fetching vehicle, #getOBD', err)
                 res.status(400).json('Error fetching vehicle')
-                return;
+                return
             }
-            console.log(vehi);
+            console.log(vehi)
             Obd.find({
                 "smidivID": vehi.smidivID,
             }).exec(function (err, pid){
                 if(err||!pid){
                     console.log('Error fetching obd, #getOBD', err)
                     res.status(400).json('Error fetching OBD')
-                    return;
+                    return
                 }
                 res.status(200).json({
                     success: true,
                     response: {
                         OBD:pid
                     }
-                });
-            });
-
-        });
+                })
+            })
+        })
     }
 }
--- api/controllers/ubicacion.js
+++ api/controllers/ubicacion.js
@@ -1,22 +1,16 @@
 'use strict'
 
 const admin = require('firebase-admin')
-const FCM = require('fcm-node')
-const turf = require ('turf')
 
-const config = require('../../config') // get our config file
-const Alarma = require('../models/alarma.js')
 const User = require('../models/user.js')
 const Vehiculo = require('../models/vehiculo.js')
 const Ubicacion = require('../models/ubicacion.js')
-const UbicacionFav = require('../models/ubicacionesFavoritas.js')
 
+const CheckAlarmsAndSend = require('../helpers/alarm_sender')
 
-const fcm = new FCM(config.firebase.key)
 
 module.exports = {
-    addUbicacion: function(req,res) {
-
+    addUbicacion: function(req, res) {
         const ubicacionObject = req.swagger.params.ubicacion.value
         if (!ubicacionObject) res.status(400).json('Error')
 
@@ -34,7 +28,8 @@ module.exports = {
                 return
             }
 
-            ubicacionObject.idAutomovil=vehiculo._id
+            ubicacionObject.idAutomovil = vehiculo._id
+            ubicacionObject.fechaCreacion = new Date()
 
             // Guardamos la ubicacion
             new Ubicacion(ubicacionObject).save(function (err, nuevaUbicacion) {
@@ -51,50 +46,7 @@ module.exports = {
                 }
             })
 
-            // Buscamos las alarmas activas para dicho usuario/vehiculo
-            Alarma.find({
-                "usuario": vehiculo.usuario._id,
-                "vehiculo": vehiculo._id,
-                "estado": true
-            }).exec(function(err,alarms) {
-                console.log(alarms)
-                // console.log("alarma" + alarm.rangoDistancia.rango)
-                //TODO: No deberiamos buscar todas las ubicaciones favs?
-                // UbicacionFav.findOne({
-                //     "idusuario": vehiculo.usuario._id
-                // })
-                // .exec(function(err, fav){
-                //     const P1 = turf.point([fav.ubicacion.lat, fav.ubicacion.lng])
-                //     const P2 = turf.point([ubicacionObject.ubicacion.lat, ubicacionObject.ubicacion.lng])
-                //     const distanciaP1P2 = turf.distance(P1, P2, 'kilometers')
-                    
-                //     if (distance > alarm.rangoDistancia.rango) {
-
-                //         console.log('Enviando notificacion de alarma activada por distancia')
-                //         const message = {
-                //             to: '/topics/' + vehiculo.usuario.username,
-                //             notification: {
-                //                 title: 'Notificacion SMIDIV!', 
-                //                 body: 'Tu Vehiculo ha salido de el rango definido' 
-                //             },
-                //             // También podemos enviar datos
-                //             // data: {
-                //             //     my_key: 'my value',
-                //             //     my_another_key: 'my another value'
-                //             // }
-                //         }
-
-                //         // Enviando notificacion
-                //         fcm.send(message, function(err, response){
-                //             if (err) {
-                //                 console.log("Something has gone wrong!")
-                //             } else {
-                //                 console.log("Successfully sent with response: ", response)
-                //             }
-                //         })
-                //     }
-                // })
-            })
+            CheckAlarmsAndSend(vehiculo._id, vehiculo.usuario._id, ubicacionObject)
         })
     },
     getUbicacion: function(req, res){
